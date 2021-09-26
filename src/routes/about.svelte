0af1117259f6f06e6ccd839337ece4c6ad1ed628
Josep Servat <jservatlorca@gmail.com>
2021-09-26 11:26:11 +0200

    initial commit with search pokemon

---
+++ src/routes/about.svelte
@@ -0,0 +1,15 @@
+<script>
+    console.log('about')
+</script>
+<svelte:head>
+    <title>About the Svelte Kit Pokdex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p>This is a really cool Svelte Kit demo that uses the Pokemon Api to build a Pokdex</p>
+<style>
+    h1 {
+        color: lightseagreen;
+    }
+</style>
+ 
---
+++ src/routes/index.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { pokemon } from '../storage/pokestore';
+    import PokemanCard from '../components/pokemanCard.svelte';
+import About from './about.svelte';
+
+    let searchTerm = "";
+    let filteredPokemon = [];
+
+    $: {
+        if(searchTerm) {
+            filteredPokemon = $pokemon.filter(pokeman => {
+                return pokeman.name.includes(searchTerm);
+            })
+        } else {
+            filteredPokemon = [...$pokemon ]
+        }
+    }
+</script>
+<svelte:head>
+    <title>Svelte Kit Pokdex</title>
+</svelte:head>
+<h1 class="text-4xl text-center my-8 uppercase">Svelte kit polkadex</h1>
+
+<input class="w-full rounder-md text-lg p-4 border-2 border-gray-200" bind:value={searchTerm} type="text" placeholder="Search Pokemon"/>
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+    {#each filteredPokemon as pokeman}
+        <PokemanCard pokeman={pokeman} />
+    {/each}
+</div>
+
+<style>
+    h1 {
+        color: lightseagreen;
+    }
+</style>
